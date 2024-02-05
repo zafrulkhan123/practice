0f8ef176918eafd7f0d29d290469afc6ee5de87d
Zafrulkhan <rbca22144.rcca@gmail.com>
2024-02-05 10:57:48 +0530

    Practice

---
+++ Practice144/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author bca
+ *
+ */
+module Practice144 {
+}
---
+++ Practice144/src/project/Caterpilar_xxx.java
@@ -0,0 +1,9 @@
+package project;
+
+public class Caterpilar_xxx {
+	int moves,cabbages;
+	Caterpilar_xxx(int   moves,  int  cabbages){
+		this.moves=moves;
+		this.cabbages=cabbages;
+}
+}
